42c5c2e301214e4fb3f83eb0c5e6439b602e053a
Nathan.Wang <Nathan.Wang@activenetwork.com>
2020-06-08 14:52:44 +0800

    update promise practising

--- my-promise/index.js
+++ my-promise/index.js
@@ -1,48 +1,70 @@
 document.addEventListener('DOMContentLoaded', function () {
   const $ = selector => document.querySelector(selector);
 
+  /**
+   * - constructor accept a fn as parameter
+   * - fn has two pre-defined parameter, the first one for resolve, while the second for reject,
+   * `resolve` or `reject` can change the internal state of promise instance
+   * - instance has `then`, `catch`, `finally` method
+   *   - `then` has two parameter, `resolvedFn` and `rejectedFn`; `resolvedFn` has the value returned by internal __resolveFn, so does rejectedFn.
+   * - constructor(Promise) has static method `race`, `all`
+   */
+
+  const PENDING = 'pending';
+  const FULLFILLED = 'fullfilled';
+  const REJECTED = 'rejected';
   class MyPromise {
-    constructor (executor) {
-      this.value = null;
-      this.stage = 'pending';
-      this.resolvedCallbacks = [];
-      this.executor = executor;
-      executor(this.resolve.bind(this));
-    }
-
-    resolve(value) {
-      if (this.stage === 'pending') {
-        this.stage = 'resolved';
-        this.resolvedCallbacks.forEach(cb => {
-          console.log('cb in resolve', cb);
-          console.log('cb value', value);
-          const retOfCb = cb(this.value);
-          this.value = retOfCb;
-          console.log('this.value', this.value);
-        });
-      }
-    }
-
-    reject() {
-      /** to be done */
-    }
-
-    then(cb) {
-      cb = typeof cb === 'function' ? cb : function () { return arguments[0] };
-      console.log(cb);
-      const retOfCb = cb(this.value);
-      console.log('retOfCb', retOfCb);
-      console.log('this.resolvedCallbacks', this.resolvedCallbacks);
-      if (this.stage === 'pending') {
-        console.log('pending');
-        this.resolvedCallbacks.push(cb);
-        console.log('this.resolvedCallbacks', this.resolvedCallbacks);
-        return this;
+    constructor(register) {
+      this.__state = PENDING; /** default state */
+      this.__successHandlers = []; /** resolved handlers */
+      this.__failureHandlers = []; /** rejected handlers */
+      this.__value = undefined;
+      const instance = this;
+      const __resolve = function(resolvedValue) {
+        console.log('instance', instance);
+        if (instance.__state === PENDING || instance.__state === FULLFILLED) {
+          instance.__state = FULLFILLED;
+          instance.__value = resolvedValue;
+          while (instance.__successHandlers.length) {
+            console.log('instance in while', instance);
+            const param = instance.__value;
+            const handler = instance.__successHandlers.shift();
+            instance.__value = handler(param);
+          }
+        }
       }
 
-      if (this.stage === 'resolved') {
-        console.log('resolved');
+      const __reject = function(rejectedReason) {
+        if (this.__state === PENDING || this.__state === REJECTED) {
+          this.__state = REJECTED;
+          while (this.__failureHandlers.length) {
+            const handler = this.__failureHandlers.shift();
+            handler(rejectedReason);
+          }
+        }
       }
+
+      register(__resolve, __reject);
+    }
+
+    then(resolvedHandler, rejectedHandler) {
+      if (typeof resolvedHandler === 'function') {
+        this.__successHandlers.push(resolvedHandler);
+      }
+
+      if (typeof rejectedHandler === 'function') {
+        this.__failureHandlers.push(rejectedHandler);
+      }
+
+      return this;
+    }
+
+    catch() {
+
+    }
+
+    finally() {
+
     }
   };
 
