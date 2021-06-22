f01de7768c044e8b7c2a660d9e9ff296a6a24c57
Nathan Wang <nathan.wang@activenetwork.com>
2021-06-22 15:13:59 +0800

    update

--- throttle/practise.js
+++ throttle/practise.js
@@ -1,40 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
   const $ = selector => document.querySelector(selector);
 
-  const throttle = function(fn, threshold) {
-    let self = null;
-    let args = [];
-    let func = null;
-    let result = null;
-    let timer = null;
-    let time_previous = +new Date();
-    let time_current = 0;
+  function throttle(func, threshold) {
+    let context = null;
+    let timer = undefined;
+    let previous = 0;
 
-    const later = function() {
+    const callback = function(...args) {
+      previous = +new Date();
       timer = null;
-      time_previous = +new Date();
-      result = fn.apply(self, args);
-    };
+      func.call(context, ...args);
+    }
 
-    return function() {
-      time_current = +new Date();
-      self = this;
-      args = arguments;
-      const duration = time_current - time_previous;
-      if (duration > threshold) {
-        if (timer) {
-          clearTimeout(timer);
-          timer = null;
-        }
-        time_previous = time_current;
-        result = fn.apply(self, args);
-      } else if (!timer) {
-        timer = setTimeout(later, threshold);
+    function closure(...args) {
+      context = this;
+      if (+new Date() - previous < threshold) {
+        if (timer) return;
+        const delay = threshold - (+new Date() - previous);
+        timer = setTimeout(callback, delay);
+      } else {
+        previous = +new Date();
+        func.call(context, ...args);
       }
+    }
 
-      return result;
-    };
-  };
+    return closure;
+  }
 
   const testFunc = param => console.log('clicked param: ', param);
   const throttled = throttle(testFunc, 1000);
